b2f62b67b5a5c82eb8e4701b9d19557902cfa086
dongwook-han <dongook.han@oliveyoung.co.kr>
2026-01-28 12:43:07 +0900

    refactor: Customize Timber debug logging

--- app/src/main/java/com/hdw/bookmarker/initializer/AppInitializer.kt
+++ app/src/main/java/com/hdw/bookmarker/initializer/AppInitializer.kt
@@ -3,13 +3,14 @@ package com.hdw.bookmarker.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.hdw.bookmarker.BuildConfig
+import com.hdw.bookmarker.core.log.TimberDebugTree
 import timber.log.Timber
 
 class AppInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
         if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+            Timber.plant(TimberDebugTree())
         }
     }
 
