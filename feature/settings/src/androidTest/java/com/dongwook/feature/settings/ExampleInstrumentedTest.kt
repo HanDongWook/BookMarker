9e48c7a7f511c5dbfb551fa17fb49f7b2dd4d5a2
HanDongWook <jja9312345@gmail.com>
2026-02-14 17:38:19 +0900

    Move SettingsScreen to settings feature module

--- feature/settings/src/androidTest/java/com/dongwook/feature/settings/ExampleInstrumentedTest.kt
+++ feature/settings/src/androidTest/java/com/dongwook/feature/settings/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.dongwook.feature.settings
+package com.hdw.bookmarker.feature.settingsetting
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dongwook.feature.settings.test", appContext.packageName)
+        assertEquals("com.hdw.bookmarker.feature.settingsetting.test", appContext.packageName)
     }
 }
