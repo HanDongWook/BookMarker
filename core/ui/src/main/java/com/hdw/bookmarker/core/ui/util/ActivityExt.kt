04cb09afa6b6548734616ebf7bdd033cea2250dc
HanDongWook <jja9312345@gmail.com>
2026-02-15 18:15:22 +0900

    Update `startFilePicker` to accept a dynamic MIME type in `ActivityExt.kt`

--- core/ui/src/main/java/com/hdw/bookmarker/core/ui/util/ActivityExt.kt
+++ core/ui/src/main/java/com/hdw/bookmarker/core/ui/util/ActivityExt.kt
@@ -5,9 +5,9 @@ import android.content.Intent
 
 const val PICK_FILE_REQUEST_CODE = 10000
 
-fun Activity.startFilePicker() {
+fun Activity.startFilePicker(mimeType: String) {
     val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-        type = "text/html" // HTML 파일만 필터링
+        type = mimeType
         addCategory(Intent.CATEGORY_OPENABLE)
     }
     startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
