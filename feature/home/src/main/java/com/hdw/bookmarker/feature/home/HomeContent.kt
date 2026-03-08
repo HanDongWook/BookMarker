8870c9423afb8fa2bd36cafbc0c2bde61a524903
HanDongWook <jja9312345@gmail.com>
2026-03-08 21:55:15 +0900

    Update FloatingActionButton colors in HomeContent and add BookMarkerYellow to theme colors

--- feature/home/src/main/java/com/hdw/bookmarker/feature/home/HomeContent.kt
+++ feature/home/src/main/java/com/hdw/bookmarker/feature/home/HomeContent.kt
@@ -19,10 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.hdw.bookmarker.core.designsystem.theme.BookMarkerYellow
 import com.hdw.bookmarker.core.model.bookmark.BookmarkItem
 import com.hdw.bookmarker.core.ui.R
 import com.hdw.bookmarker.feature.home.appbar.HomeTopAppBar
@@ -75,6 +77,8 @@ internal fun HomeContent(
             FloatingActionButton(
                 onClick = onAddItemClick,
                 shape = CircleShape,
+                containerColor = BookMarkerYellow,
+                contentColor = Color.Black,
             ) {
                 Icon(
                     imageVector = Icons.Default.Add,
