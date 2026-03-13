7471f6a25679ba4784ab9186552d1de8b5a320a7
dongwook-han <dongook.han@oliveyoung.co.kr>
2026-03-13 14:00:10 +0900

    Reformat code across various modules to follow consistent styling guidelines

--- feature/home/src/main/java/com/hdw/bookmarker/feature/home/contract/AddBookmarkItemRequest.kt
+++ feature/home/src/main/java/com/hdw/bookmarker/feature/home/contract/AddBookmarkItemRequest.kt
@@ -4,15 +4,9 @@ sealed interface AddBookmarkItemRequest {
     val parentFolderPath: List<Int>?
     val title: String
 
-    data class Bookmark(
-        override val parentFolderPath: List<Int>?,
-        override val title: String,
-        val url: String,
-    ) : AddBookmarkItemRequest
+    data class Bookmark(override val parentFolderPath: List<Int>?, override val title: String, val url: String) :
+        AddBookmarkItemRequest
 
-    data class Folder(
-        override val parentFolderPath: List<Int>?,
-        override val title: String,
-        val description: String,
-    ) : AddBookmarkItemRequest
+    data class Folder(override val parentFolderPath: List<Int>?, override val title: String, val description: String) :
+        AddBookmarkItemRequest
 }
